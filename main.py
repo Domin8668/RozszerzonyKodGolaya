062f354f088bdfb2a778ba4eb13728071b20fca8
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2022-01-31 15:30:21 +0100

    Improved console representation. Now showing more detailed steps.

--- main.py
+++ main.py
@@ -15,7 +15,20 @@ def get_s(u1, u2):
                   [0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1],
                   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
     s = u1.transpose() + np.matmul(b, u2.transpose())
+    print(f'  u1={list(u1)}')
+    print(f'b*u2={list(map(lambda x: x % 2, np.matmul(b, u2.transpose())))}')
+    print(f'+{(len(f"{u1=}") - 6) * "_"}')
     return list(map(lambda x: x % 2, s))
+
+
+# Funkcja pomocnicza; nie jest wykorzystywana w main()
+def check_s(c1, c2):
+    # Sprawdzamy, czy c jest poprawnym ciągiem:
+    s = get_s(np.array(c1), np.array(c2))
+    if sum(s):
+        print("Wektor nie jest poprawny.")
+    else:
+        print("Wektor jest poprawny.")
 
 
 def get_e(s1):
@@ -32,34 +45,41 @@ def get_e(s1):
                   [0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1],
                   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
 
+    # Kolumny macierzy b sa numerowane od 1 do 12.
     w1 = sum(s1)
-    print(f'{w1=}')
     if w1 <= 3:
+        print(f'{w1=}<=3\n')
         return [s1, 12 * [0]]
+    print(f'{w1=}>3')
     for i, bi in enumerate(b):
         s1_add_bi = [(x + y) % 2 for x, y in zip(s1, bi)]
         ei = 12 * [0]
         ei[i] = 1
         if sum(s1_add_bi) <= 2:
             i += 1
-            print(f'{s1_add_bi=}\n{i=}')
+            print(f's1+b{i}={sum(s1_add_bi)}<=2\n')
             return [s1_add_bi, ei]
+        print(f's1+b{i}={sum(s1_add_bi)}>2')
+    print("\n")
 
     s2 = np.matmul(b, s1.transpose())
     s2 = list(map(lambda x: x % 2, s2))
 
     w2 = sum(s2)
-    print(f'{w2=}')
     if w2 <= 3:
+        print(f'{w2=}<=3\n')
         return [12 * [0], s2]
+    print(f'{w2=}>3')
     for i, bi in enumerate(b):
         s2_add_bi = [(x + y) % 2 for x, y in zip(s2, bi)]
         ei = 12 * [0]
         ei[i] = 1
         if sum(s2_add_bi) <= 2:
             i += 1
-            print(f'{s2_add_bi=}\n{i=}')
+            print(f's2+b{i}={sum(s2_add_bi)}<=2\n')
             return [ei, s2_add_bi]
+        print(f's2+b{i}={sum(s2_add_bi)}>2')
+    print("\n")
     return None
 
 
@@ -69,32 +89,23 @@ def correct_u(u, e):
 
 
 def main():
-    # Sprawdzamy, czy c jest poprawnym ciągiem:
-    c1 = [0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0]
-    c2 = [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1]
-    s = get_s(np.array(c1), np.array(c2))
-    print(f'{s=}')
-    if sum(s):
-        print("Wektor nie jest poprawny.")
+    u1 = [0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1]
+    u2 = [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0]
+    u = [u1, u2]
+    # Obliczamy syndrom s1:
+    s1 = get_s(np.array(u1), np.array(u2))
+    print(f'  {s1=}\n')
+    # Obliczamy korekcję e dla u:
+    e = get_e(np.array(s1))
+    print(f'{u=}')
+    print(f'{e=}')
+    if e:
+        # Poprawiamy ciąg u:
+        c = correct_u(u, e)
+        print(f'+{(len(f"{c=}") - 1) * "_"}')
+        print(f'{c=}')
     else:
-        print("Wektor jest poprawny.")
-        u1 = [0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1]
-        u2 = [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0]
-        u = [u1, u2]
-        # Obliczamy syndrom s1:
-        s1 = get_s(np.array(u1), np.array(u2))
-        print(f'{s1=}')
-        # Obliczamy korekcję e dla u:
-        e = get_e(np.array(s1))
-        print(f'{u=}')
-        print(f'{e=}')
-        if e:
-            # Poprawiamy ciąg u:
-            c = correct_u(u, e)
-            print(f'+{(len(f"{c=}") - 1) * "_"}')
-            print(f'{c=}')
-        else:
-            print("Nie można skorygować ciągu.")
+        print("Nie można skorygować ciągu.")
 
 
 if __name__ == "__main__":
